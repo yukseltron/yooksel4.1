f3a004e8083434eb882af7e5548e1b476cc75a87
yukseltron <h.yuksel04@gmail.com>
2021-03-26 16:44:40 -0400

    added updates

--- src/Marqet.js
+++ src/Marqet.js
@@ -84,12 +84,7 @@ class Marqet extends Component {
               <h3>Overview</h3>
               <p class="center-content spacer">Marqet was inspired by art markets that are available online, but are somewhat complex to the average user since it deals with stuff such as crypto currencies, blockchain, hashes, and more. An easier to understand market place, meant for simply auctioning, browsing, buying/selling, and showcasing collections, would be different to the existing crypto markets that remain ‘cryptic’ (not showing identities, collections, limited socializing). Although this is intentional to create a more anonymous ecosystem, it can be scary to newcomers. So, Marqet’s usefulness is in trying to appeal to newcomers interested in understand more of the crypto world and its practicalities.</p>
           </div>
-          <div class="border-bottom spacer" ref={this.div1}>
-              <div class="circlebox">
-                  <div class="div1 filled"></div>
-                  <div class="div2"></div>
-                  <div class="div3"></div>
-              </div>
+          <div class="border-top spacer" ref={this.div1}>
               <h3>Introduction</h3>
               <br/>
               <br/>
@@ -99,8 +94,9 @@ class Marqet extends Component {
                       <div class="problem-section spacer">
                           <img class="img-s" class="problem" src={challenge}/>
                           <div>
-                              <h2>The Challenge</h2>
-                              <p class="p-left center-content">How can we provide a mobile art market for any art enthusiast wondering what they can do with their crypto funds?</p>
+                              <h2 class="text-left center-content">The Challenge</h2>
+                              <br/>
+                              <p class="text-left center-content">How can we provide a mobile art market for any art enthusiast wondering what they can do with their crypto funds?</p>
                           </div>
                       </div>
                   </div>
@@ -139,12 +135,7 @@ class Marqet extends Component {
                   </div>
               </div>
           </div>
-          <div class="border-bottom section spacer" ref={this.div2}>
-              <div class="circlebox">
-                  <div class="div1"></div>
-                  <div class="div2 filled"></div>
-                  <div class="div3"></div>
-              </div>
+          <div class="border-top section spacer" ref={this.div2}>
               <div>
       			<h3>Research</h3>
                   <p class="center-content">Doing some secondary research suggests that as crypto has grown massively in popularity, markets have emerged that try to sell things through crypto. Sometimes the things they sell are encrypted in a blockchain too as NFTs. Virtually any sort of file can be authenticated 'minted' to become an NFT.</p>
@@ -153,21 +144,24 @@ class Marqet extends Component {
                   <p class="center-content">Example of an NFT market: <a target="_blank" href="https://www.nbatopshot.com/">nbatopshot.com</a></p>
                   <br/>
           	  	<br/>
-                <div class="horizontal-section center-content">
-                    <img class="img-m" alt="art work titled cryptopunks, generative art and among the first NFTs minted" src={research} />
-                    <p>Cryptopunks were amongst the first NFTs made. It was a generative art project made by 2 people called Larva Labs. You can read more about them <a href="https://www.larvalabs.com/cryptopunks">in this article. </a></p>
+                <div class="">
+                    <p class="center-content">Cryptopunks were amongst the first NFTs made. It was a generative art project made by 2 people called Larva Labs. You can read more about them <a href="https://www.larvalabs.com/cryptopunks">in this article.</a>  Some examples are shown in the screenshot below.</p>
+                    <br/>
+                    <br/>
+                    <img class="img-s" alt="art work titled cryptopunks, generative art and among the first NFTs minted" src={research} />
                 </div>
               </div>
   			<img class="spacer" />
               <div class="spacer">
       			<h2>Persona</h2>
-                  <p class="p-center">Here's a persona for this project.</p>
+                  <p class="center-content">Here's a persona for this project, made through secondary research and a user interview with a small-time art collector.</p>
                   <br/>
                   <br/>
-                  <div class="horizontal-section center-content border-padding">
-                      <img class="border center-content margin-right img-m" src={persona} />
+                  <div class="horizontal-section center-content border-padding subtle-shadow">
+                      <img class="border-white center-content margin-right img-m" src={persona} />
                         <div>
                           <h3 class="text-left">Hailey</h3>
+                          <h4 class="text-left">Age: 35</h4>
                           <h4 class="text-left bottom-border-white">Art enthusiast, Crypto newcomer</h4>
                           <h5 class="text-left">Hailey has cryptocurrency in the form of ethereum, but doesn’t really know what to do with it. <br/><br/>She’s interested in art,
       and how they can be purchased as NFT’s,
@@ -184,24 +178,19 @@ class Marqet extends Component {
                 <img  src={ideation}/>
               </div>
   		</div>
-        <div class="circlebox" ref={this.div4}>
-            <div class="div1"></div>
-            <div class="div2"></div>
-            <div class="div3 filled"></div>
-        </div>
 		<div class="section spacer">
         <br/>
-            <div class="spacer">
+            <div class="spacer border-top">
     			<h3>Prototyping</h3>
                 <h2>Midfi</h2>
-                <p class="p-center">With the research I had done, I made some midfi screens to better test out the ideas.</p>
+                <p class="center-content">Using the research and sketches I had done, I made some midfi screens to better test out the ideas.</p>
                 <br/>
                 <br/>
                 <img  src={midfi}/>
             </div>
             <div class="spacer final-design-section">
                 <h2>Hifi</h2>
-                <p class="p-center">Next up, I designed a hi-fidelity prototype.</p>
+                <p class="center-content">Next up, after getting peer-reviewed and taking notes of improvements, I designed a hi-fidelity prototype.</p>
                 <img src={hifi}/>
                 <br/>
                 <br/>
@@ -209,7 +198,7 @@ class Marqet extends Component {
                 <br/>
                 <br/>
                 <br/>
-                <p class="p-center">Here is the design and accessibility specifications I arranged for it.</p>
+                <p class="p-center">Here are the design and accessibility specifications I arranged for it.</p>
                 <br/>
                 <br/>
                 <br/>
@@ -223,6 +212,7 @@ class Marqet extends Component {
                 <div class="spacer">
                     <div>
                         <h2 class="center-content p-left">Artwork Info</h2>
+                        <br/>
                         <h4 class=" center-content storyboard-h4">In this scenario, our user wants to find an artwork of interest,
 learn more about it, and decide
 to keep it on a watch list.</h4>
@@ -236,6 +226,7 @@ to keep it on a watch list.</h4>
                 <div class="spacer">
                     <div>
                         <h2 class="center-content p-left">Auctions</h2>
+                        <br/>
                         <h4 class="center-content  storyboard-h4">Here, our user found an interesting piece of art
 and now wants to place a bid to buy it
 in auction.</h4>
@@ -249,6 +240,7 @@ in auction.</h4>
                 <div class="">
                     <div>
                         <h2 class="center-content  p-left">Adjust Privacy</h2>
+                        <br/>
                         <h4 class="center-content  storyboard-h4">Now our user wants to adjust the privacy
 settings for an art piece in her collection.
 She wants it to be hidden from the public.</h4>
@@ -268,7 +260,7 @@ She wants it to be hidden from the public.</h4>
             <br/>
             <div class="border-bottom border-top spacer">
                 <h2 class="p-center">Check it out</h2>
-                <h4 class="p-center"><a target="_blank" href="https://www.figma.com/file/kXyfQVuJfCT81cC5NLzLNg/A2?node-id=0%3A1">View on figma</a></h4>
+                <h4 class="p-center"><a target="_blank" href="https://www.figma.com/file/kXyfQVuJfCT81cC5NLzLNg/A2?node-id=0%3A1">View figma</a></h4>
             </div>
             <br/>
             <br/>
@@ -279,9 +271,13 @@ She wants it to be hidden from the public.</h4>
             <div class="spacer">
                 <h2>Reflection</h2>
                 <p class="center-content">This was a fun project for learning more about NFTs, and blockchains.</p>
+                <br/>
                 <p class="center-content">In terms of enivormental impact, it was interesting to learn about Proof of Work vs Proof of Stake in crypto transactions. Proof of stake has promise in terms of being more energy efficient, and is seeing widespread usage.</p>
+                <br/>
                 <p class="center-content">Although cryptocurrencies generally pose a terrible risk in terms of the environmental cost that comes with them, proof of stake mining could help overcome that.</p>
+                <br/>
                 <p class="center-content">However, time will tell how the larger art world will react to NFTs as they become more and more popular.</p>
+                <br/>
                 <p class="center-content">As such, for the future, it would be good to focus on adding more socially-conscious aspects such as the carbon footprint of a crypto artwork.</p>
                 <br/>
                 <br/>
